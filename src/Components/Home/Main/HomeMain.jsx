3692291172f4b56782db86218a714e3c21b99b51
PozitGo <pozitgo2005@gmail.com>
2023-11-17 15:19:04 +0200

    add slider banner

--- src/Components/Home/Main/HomeMain.jsx
+++ src/Components/Home/Main/HomeMain.jsx
@@ -1,26 +1,12 @@
 import styles from "./HomeMain.module.css";
-import PizzaBanner from "../../../Images/PizzaBaner.png";
-
+import PhotoSlider from "../../UI/PhotoSlider";
 import Search from "../../../Images/search.png";
 import DishList from "../DishList/DishList";
 
 const HomeMain = () => {
   return (
     <section>
-      <div className={styles.CenterContainer}>
-        <section className={styles.BackgroundBanner}>
-          <div className={styles.HeaderTextContain}>
-            <h1>Онлайн-сервис</h1>
-            <h1>доставки еды на дом</h1>
-            <section className={styles.BannerDescription}>
-              <p>Блюда из любимого ресторана привезет</p>
-              <p>курьер в перчатках, маске и с антисептиком</p>
-            </section>
-          </div>
-          <img src={PizzaBanner} about="Banner" />
-        </section>
-      </div>
-
+      <PhotoSlider />
       <section className={styles.RestaurantContain}>
         <section className={styles.RestaurantHeader}>
           <h1>Рестораны</h1>
