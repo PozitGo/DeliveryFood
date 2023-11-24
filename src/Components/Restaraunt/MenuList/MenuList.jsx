9064c67877133db49ec2eabc3c25945340ac7dfa
PozitGo <pozitgo2005@gmail.com>
2023-11-24 14:32:38 +0200

    add 4.5

--- src/Components/Restaraunt/MenuList/MenuList.jsx
+++ src/Components/Restaraunt/MenuList/MenuList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Menu from "../Menu/RestarauntMenu";
-
+import menuData from "../../../Data/TanukiMenuData.json";
+import { useParams } from "react-router-dom";
 import image1 from "../../../Images/Menu/image1.png";
 import image2 from "../../../Images/Menu/image2.png";
 import image3 from "../../../Images/Menu/image3.png";
@@ -10,67 +11,38 @@ import image6 from "../../../Images/Menu/image6.png";
 
 import styles from "./MenuList.module.css";
 
-const MenuData = [
-  {
-    id: 1,
-    name: "Ролл угорь стандарт",
-    image: image1,
-    Description: "Рис, угорь, соус унаги, кунжут, водоросли нори.",
-    Price: 250,
-  },
-  {
-    id: 2,
-    name: "Калифорния лосось стандарт",
-    image: image2,
-    Description:
-      "Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.",
-    Price: 395,
-  },
-  {
-    id: 3,
-    name: "Окинава стандарт",
-    image: image3,
-    Description:
-      " Рис, креветка отварная, сыр сливочный, лосось, огурец свежий...",
-    Price: 250,
-  },
-  {
-    id: 4,
-    name: "Цезарь маки хl",
-    image: image4,
-    Description:
-      "Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...",
-    Price: 250,
-  },
-  {
-    id: 5,
-    name: "Ясай маки стандарт 185 г",
-    image: image5,
-    Description:
-      "Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг",
-    Price: 250,
-  },
-  {
-    id: 6,
-    name: "Ролл с креветкой стандарт",
-    image: image6,
-    Description:
-      "Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы",
-    Price: 250,
-  },
+import FoodBandMenuData from "../../../Data/FoodBandMenuData.json";
+import FoodPointMenuData from "../../../Data/FoodPointMenuData.json";
+import GreedyPizzaMenuData from "../../../Data/GreedyPizzaMenuData.json";
+import PizzaBurgerMenuData from "../../../Data/PizzaBurgerMenuData.json";
+import PizzaPlusMenuData from "../../../Data/PizzaPlusMenuData.json";
+import TanukiMenuData from "../../../Data/TanukiMenuData.json";
+
+const images = [image1, image2, image3, image4, image5, image6];
+const allMenus = [
+  TanukiMenuData,
+  FoodBandMenuData,
+  FoodPointMenuData,
+  GreedyPizzaMenuData,
+  PizzaBurgerMenuData,
+  PizzaPlusMenuData,
 ];
 
 const MenuList = () => {
+  const { restaurantId } = useParams();
+  const menuIndex = parseInt(restaurantId, 10) - 1;
+  const menuData = allMenus[menuIndex] || [];
+
   return (
     <section className={styles.Contain}>
-      {MenuData.map((item) => (
+      {menuData.map((item) => (
         <Menu
           key={item.id}
           id={item.id}
-          img={item.image}
-          Name={item.name}
-          Description={item.Description}
-          Price={item.Price}
+          img={images[item.id - 1]}
+          name={item.name}
+          description={item.description}
+          price={item.price}
         />
       ))}
     </section>
