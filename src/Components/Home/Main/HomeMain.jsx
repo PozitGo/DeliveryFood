bef31ce9b79b5ce521435748e47a8a032d22f2c8
PozitGo <pozitgo2005@gmail.com>
2023-12-01 13:22:31 +0200

    add 4.6

--- src/Components/Home/Main/HomeMain.jsx
+++ src/Components/Home/Main/HomeMain.jsx
@@ -2,8 +2,15 @@ import styles from "./HomeMain.module.css";
 import PhotoSlider from "../../UI/PhotoSlider";
 import Search from "../../../Images/search.png";
 import DishList from "../DishList/DishList";
+import { useState } from "react";
 
 const HomeMain = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <section>
       <PhotoSlider />
@@ -12,11 +19,15 @@ const HomeMain = () => {
           <h1>Рестораны</h1>
           <section className={styles.RestaurantSearch}>
             <img src={Search} alt="Search" />
-            <input placeholder="Поиск блюд и ресторанов" />
+            <input
+              placeholder="Поиск блюд и ресторанов"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
           </section>
         </section>
 
-        <DishList />
+        <DishList searchTerm={searchTerm} />
       </section>
     </section>
   );
