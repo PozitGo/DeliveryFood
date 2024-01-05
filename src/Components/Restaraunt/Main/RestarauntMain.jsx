32941f8680912b601108e0e7b46a1941500a630b
PozitGo <pozitgo2005@gmail.com>
2024-01-05 22:23:39 +0200

    add 5_7

--- src/Components/Restaraunt/Main/RestarauntMain.jsx
+++ src/Components/Restaraunt/Main/RestarauntMain.jsx
@@ -1,32 +1,45 @@
-import styles from "./Restaraunt.module.css";
-import Rating from "../../../Images/RatingIMG.png";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import MenuList from "../MenuList/MenuList";
-import restaurantsData from "../../../Data/dishData.json";
-import { useState, useEffect } from "react";
+
+import styles from "./Restaraunt.module.css";
+import Rating from "../../../Images/RatingIMG.png";
+
 const RestarauntMain = () => {
   let { restaurantId } = useParams();
   const [restaurant, setRestaurant] = useState(null);
 
   useEffect(() => {
     const id = parseInt(restaurantId, 10);
-    const foundRestaurant = restaurantsData.find((r) => r.id === id);
-    setRestaurant(foundRestaurant);
+    axios
+      .get(
+        `https://deliveryfood-2f0eb-default-rtdb.asia-southeast1.firebasedatabase.app/dishList.json`
+      )
+      .then((response) => {
+        setRestaurant(response.data);
+      })
+      .catch((error) => {
+        console.error("Ошибка при загрузке данных ресторана:", error);
+      });
   }, [restaurantId]);
 
   if (!restaurant) {
-    return <div>Loading...</div>;
+    return <div className={styles.Loading}>Loading...</div>;
   }
 
   return (
     <section className={styles.MainContainer}>
       <section className={styles.HeaderRestoraunt}>
         <h1>{restaurant.name}</h1>
-        <img src={Rating} />
-        <p className={styles.Rating}>4.5</p>
-        <p className={styles.Price}>От 900 ₽</p>
+        <img src={Rating} alt="Рейтинг" />
+        <p className={styles.Rating}>4.5</p>{" "}
+        {/* Предположительно, рейтинг будет частью данных ресторана */}
+        <p className={styles.Price}>От 900 ₽</p>{" "}
+        {/* Аналогично, цена может быть частью данных */}
         <ul>
-          <li>Пицца и суши</li>
+          <li>Пицца и суши</li>{" "}
+          {/* Эти данные также должны приходить от API, если они переменны */}
         </ul>
       </section>
       <MenuList />
